221c4bb98cb776e8a1529a9cc8b3527334920998
wucucu <wucucu@users.noreply.github.com>
2019-07-04 00:07:56 +0200

    Extract data.

--- scrapy_tutorial/scrapy_tutorial/spiders/quotes_spider.py
+++ scrapy_tutorial/scrapy_tutorial/spiders/quotes_spider.py
@@ -1,7 +1,7 @@
 import scrapy
 
 
-class QuotesSpider(scrapy.Spider):
+class QuotesSpider0(scrapy.Spider):
 
     name = "quotes"
 
@@ -19,3 +19,23 @@ class QuotesSpider(scrapy.Spider):
         with open(filename, "wb") as f:
             f.write(response.body)
         self.log(f"Save file {filename}")
+
+
+class QuotesSpider1(scrapy.Spider):
+
+    name = "quotes"
+    start_urls = [
+        "http://quotes.toscrape.com/page/1/",
+        "http://quotes.toscrape.com/page/2/",
+    ]
+
+    def parse(self, response):
+        for quote in response.css("div.quote"):
+            yield {
+                "text": quote.css("span.text::text").get(),
+                "author": quote.css("small.author::text").get(),
+                "tags": quote.css("div.tags a.tag::text").getall(),
+            }
+
+
+QuotesSpider = QuotesSpider1
